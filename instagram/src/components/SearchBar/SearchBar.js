8c9837754ae4737a8ebd39867360fff27776d194
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-14 17:38:13 -0500

    index.css CSS Reset + global styles, create SearchBar.css

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faCompass, faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
+import './SearchBar.css';
 
 import Logo from '../../img/instagram-logo.png';
 const SearchBar = () => {
     return (
-        <div>
+        <div className="search-bar">
             <FontAwesomeIcon icon={faInstagram} />
-            <img src={Logo} />
+            <img className="ig-logo" alt="Instagram logo" src={Logo} />
             <input type="text" />
             <FontAwesomeIcon icon={faCompass} />
             <FontAwesomeIcon icon={faHeart} />
