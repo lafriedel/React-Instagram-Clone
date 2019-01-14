d4ab09459ef8e93371bcccf5d88b0e99c1de22fb
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-14 17:31:49 -0500

    Add final FontAwesome dependency, import SearchBar icons

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faCompass, faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
+
 import Logo from '../../img/instagram-logo.png';
 const SearchBar = () => {
     return (
         <div>
-            <FontAwesomeIcon icon={faInstagram}/>
+            <FontAwesomeIcon icon={faInstagram} />
             <img src={Logo} />
+            <input type="text" />
+            <FontAwesomeIcon icon={faCompass} />
+            <FontAwesomeIcon icon={faHeart} />
+            <FontAwesomeIcon icon={faUser} />
         </div>
     );
 }
