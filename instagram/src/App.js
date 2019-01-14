0ac685669f9009d95c22daa507f1cc0d7914347f
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-14 15:50:30 -0500

    Moved dummy-data inside src/, mapped over dummyData to create PostContainer

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData
+    }
+  }
   render() {
+    console.log(dummyData);
     return (
       <div className="App">
-       
+        {this.state.dummyData.map(post => {
+          return (<div><PostContainer post={post} key={post.timestamp}/></div>);
+        })}
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PostContainer = props => {
+    return (
+        <div>Inside post container</div>
+    )
+}
+
+export default PostContainer;
