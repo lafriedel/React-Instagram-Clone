a566533622c029fa9d36ef2697a0051e55e61b7e
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-14 17:22:40 -0500

    Add correct FontAwesome dependencies, import IG text logo

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import Logo from '../../img/instagram-logo.png';
 const SearchBar = () => {
     return (
         <div>
-            
+            <FontAwesomeIcon icon={faInstagram}/>
+            <img src={Logo} />
         </div>
     );
 }
