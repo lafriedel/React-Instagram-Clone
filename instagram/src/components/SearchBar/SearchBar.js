ac1fe6b15809c6d87f605225e7be97cfcd8bb3b4
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-14 18:15:03 -0500

    First pass at SearchBar styling

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faCompass, faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
-import './SearchBar.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faCompass,
+  faHeart,
+  faUser
+} from "@fortawesome/free-regular-svg-icons";
+import "./SearchBar.css";
 
-import Logo from '../../img/instagram-logo.png';
+import Logo from "../../img/instagram-logo.png";
 const SearchBar = () => {
-    return (
-        <div className="search-bar">
-            <FontAwesomeIcon icon={faInstagram} />
-            <img className="ig-logo" alt="Instagram logo" src={Logo} />
-            <input type="text" />
-            <FontAwesomeIcon icon={faCompass} />
-            <FontAwesomeIcon icon={faHeart} />
-            <FontAwesomeIcon icon={faUser} />
-        </div>
-    );
-}
+  return (
+    <div className="search-bar">
+      <div className="search-logo-container">
+      <FontAwesomeIcon icon={faInstagram} />
+      <img className="ig-logo" alt="Instagram logo" src={Logo} />
+      </div>
 
+      <div className="search-input-container" placeholder="Search">
+        <input type="text" />
+      </div>
+      <div className="search-icons-container">
+        <FontAwesomeIcon icon={faCompass} />
+        <FontAwesomeIcon icon={faHeart} />
+        <FontAwesomeIcon icon={faUser} />
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
