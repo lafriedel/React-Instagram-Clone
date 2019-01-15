d27594885413328041fc5d7c1b428e4b125a29b9
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-15 15:51:07 -0500

    Implement addNewComment inside CommentSection

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -5,20 +5,65 @@ import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 import "./Comments.css";
 import PropTypes from "prop-types";
 
-const CommentSection = props => {
-  return (
-    <div>
-      <div className="comments-section">
-        {props.comments.map(comment => {
-          return <Comment comment={comment} key={1 + Math.random()} />;
-        })}
+class CommentSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+      commentText: ""
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      comments: this.props.comments
+    })
+  }
+
+  addNewComment = event => {
+    event.preventDefault();
+    this.setState({
+      comments: [
+        ...this.state.comments,
+        {
+          text: this.state.commentText,
+          username: "lafriedel"
+        }
+      ]
+    })
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    
+    return (
+      <div>
+        <div className="comments-section">
+          {this.state.comments.map(comment => {
+            return <Comment comment={comment} key={1 + Math.random()} />;
+          })}
+        </div>
+        <div className="add-comment">
+          <form onSubmit={this.addNewComment}>
+            <input 
+              name="commentText" 
+              value={this.state.commentText}
+              type="text" 
+              placeholder="Add a comment..." 
+              onChange={this.handleChange}
+            />
+          </form>
+          <FontAwesomeIcon size="2x" icon={faEllipsisH} />
+        </div>
       </div>
-      <div className="add-comment">
-        <p>Add a comment...</p>
-        <FontAwesomeIcon size="2x" icon={faEllipsisH} />
-      </div>
-    </div>
-  );
+    );
+  }
+
 };
 
 CommentSection.propTypes = {
