2a379ce1bbf565f4977191e25fb885cd4acca10d
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-15 17:07:19 -0500

    Toggle like functionality

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -3,27 +3,59 @@ import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faComment } from "@fortawesome/free-regular-svg-icons";
 
-const Post = props => {
-  return (
-    <div className="post">
-      <div className="post-header">
-        <img className="user-icon" alt="User icon" src={props.post.thumbnailUrl} />
-        <span>{props.post.username}</span>
-      </div>
+class Post extends React.Component {
 
-      <div className="post-img">
-        <img alt="User's post" src={props.post.imageUrl} />
-      </div>
-      <div className="social-activation">
-        <div className="social-activation-icons">
-          <FontAwesomeIcon size="2x" icon={faHeart} />
-          <FontAwesomeIcon size="2x" flip="horizontal" icon={faComment} />
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      userLiked: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      likes: this.props.post.likes
+    })
+  }
+
+  likePost() {
+    console.log(this.state.userLiked);
+    this.state.userLiked === false ?
+      this.setState(prevState => ({
+        likes: ++prevState.likes,
+        userLiked: true
+      })) : 
+      this.setState(prevState => ({
+        likes: --prevState.likes,
+        userLiked: false
+      }))
+  }
+
+  render() {
+    console.log(this.state.likes);
+    return (
+      <div className="post">
+        <div className="post-header">
+          <img className="user-icon" alt="User icon" src={this.props.post.thumbnailUrl} />
+          <span>{this.props.post.username}</span>
         </div>
-
-        <p>{props.post.likes} likes</p>
+  
+        <div className="post-img">
+          <img alt="User's post" src={this.props.post.imageUrl} />
+        </div>
+        <div className="social-activation">
+          <div className="social-activation-icons">
+            <FontAwesomeIcon onClick={() => this.likePost()} size="2x" icon={faHeart} />
+            <FontAwesomeIcon size="2x" flip="horizontal" icon={faComment} />
+          </div>
+  
+          <p>{this.state.likes} likes</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 };
 
 Post.propTypes = {
