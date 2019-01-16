96ec8e8b04b1cd6ed2e133f7804612eb667787bd
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-15 21:26:38 -0500

    Moved most App functionality to PostsPage, render PostsPage in App

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,63 +1,18 @@
 import React, { Component } from 'react';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
+import PostsPage from './components/PostContainer/PostsPage';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      dummyData: [],
-      searchedData: [],
-      searchTerm: ""
-    }
   }
 
-  componentDidMount() {
-    this.setState({
-      dummyData: dummyData
-    })
-  }
-
-  handleSearch = event => {
-    event.preventDefault();
-   
-      this.setState({
-        dummyData: this.state.dummyData.filter(post => {
-          return post.username.toLowerCase() === this.state.searchTerm.toLowerCase();
-        })
-      })
-    
-
-  };
-
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  
   render() {
-    
       return (
         <div className="App">
-        <SearchBar
-          dummyData={this.state.dummyData}
-          handleSearch={this.handleSearch}
-          handleChange={this.handleChange}
-          searchTerm={this.state.searchTerm}
-        />
-          {this.state.dummyData.map(post => {
-            return (
-              <PostContainer post={post} key={1 + Math.random()} />
-            );
-          })}
+        <PostsPage />
         </div>
       );
-
-
   }
 }
 
--- instagram/src/components/PostContainer/PostsPage.js
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -1,8 +1,63 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import dummyData from '../../dummy-data';
+import SearchBar from '../SearchBar/SearchBar';
+import PostContainer from './PostContainer';
+import PropTypes from "prop-types";
 
-const PostsPage = () => {
+class PostsPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: [],
+      searchedData: [],
+      searchTerm: ""
+    };
+  }
 
+  componentDidMount() {
+    this.setState({
+      dummyData: dummyData
+    });
+  }
+
+  handleSearch = event => {
+    event.preventDefault();
+
+    this.setState({
+      dummyData: this.state.dummyData.filter(post => {
+        return (
+          post.username.toLowerCase() === this.state.searchTerm.toLowerCase()
+        );
+      })
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    
+    return (
+      <div className="App">
+      <SearchBar
+        dummyData={this.state.dummyData}
+        handleSearch={this.handleSearch}
+        handleChange={this.handleChange}
+        searchTerm={this.state.searchTerm}
+      />
+        {this.state.dummyData.map(post => {
+          return (
+            <PostContainer post={post} key={1 + Math.random()} />
+          );
+        })}
+      </div>
+    );
+
+
+}
 }
 
 export default PostsPage;
