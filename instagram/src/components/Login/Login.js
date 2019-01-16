7d52b90317325863c5089ad5b663bbdc2dda55cc
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-16 18:11:20 -0500

    Style login page

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
+import Logo from "../../img/instagram-logo.png";
+import AppStore from '../../img/apple-app-store.png';
+import GooglePlay from '../../img/google-play-store.png';
+import "./Login.css";
 
 const Login = props => {
-    return (
-        <form onSubmit={props.login}>
-            <input type="text" placeholder="username" name="username" onChange={props.handleChange} />
-            <input type="text" placeholder="password" />
-            <button type="submit">Login</button>
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <img className="login-ig-logo" src={Logo} />
+        <form onSubmit={props.login} className="login-form">
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={props.handleChange}
+          />
+          <input type="text" placeholder="Password" />
+          <button type="submit">Log in</button>
         </form>
-    )
-}
+      </div>
+      <p>Get the app.</p>
+      <div className="app-buttons-container">
+        <img src={AppStore} />
+        <img src={GooglePlay} />
+      </div>
+    </div>
+  );
+};
 
 export default Login;
