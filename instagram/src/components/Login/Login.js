1f97d28710f75d0b89a804e2e855933a2d055094
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-17 16:11:39 -0500

    Refactor CSS with styled-components in Login

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -2,14 +2,80 @@ import React from "react";
 import Logo from "../../img/instagram-logo.png";
 import AppStore from '../../img/apple-app-store.png';
 import GooglePlay from '../../img/google-play-store.png';
-import "./Login.css";
+// import "./Login.css";
+import styled from 'styled-components';
+
+const LoginPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const LoginContainer = styled.div`
+  border: 1px solid lightgrey;
+  background: white;
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 15px 15px 30px;
+  margin-bottom: 25px;
+
+  img {
+    width: 180px;
+    margin-bottom: 10px;
+  }
+`;
+
+const LoginForm = styled.form`
+  width: 90%;
+
+  input[type=text] {
+    padding: 10px;
+    margin-bottom: 10px;
+    background: whitesmoke;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+    width: 100%;
+
+    &:focus {
+      outline: none;
+      border: 1px solid grey;
+    }
+  }
+
+  button {
+    padding: 8px;
+    background: dodgerblue;
+    color: white;
+    font-weight: 600;
+    font-size: 0.9rem;
+    width: 100%;
+    border: none;
+    border-radius: 5px;
+    margin-top: 10px;
+    cursor: pointer;
+  }
+`;
+
+const GetApps = styled.div`
+  margin-top: 25px;
+
+  img {
+    width: 130px;
+    margin: 0 4px;
+  }
+`;
 
 const Login = props => {
   return (
-    <div className="login-page">
-      <div className="login-container">
+    <LoginPage>
+      <LoginContainer>
         <img className="login-ig-logo" src={Logo} />
-        <form onSubmit={props.login} className="login-form">
+        <LoginForm onSubmit={props.login}>
           <input
             type="text"
             placeholder="Username"
@@ -18,14 +84,14 @@ const Login = props => {
           />
           <input type="text" placeholder="Password" />
           <button type="submit">Log in</button>
-        </form>
-      </div>
+        </LoginForm>
+      </LoginContainer>
       <p>Get the app.</p>
-      <div className="app-buttons-container">
+      <GetApps>
         <img src={AppStore} />
         <img src={GooglePlay} />
-      </div>
-    </div>
+      </GetApps>
+    </LoginPage>
   );
 };
 
